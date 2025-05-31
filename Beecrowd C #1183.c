7582b439a70b1a4705540d2372c4e4dc47022121
Luca Ryuichi Kasai Watanabe <148013742+LucaWata@users.noreply.github.com>
2025-05-31 15:14:33 -0300

    Acima da Diagonal Principal

---
+++ Beecrowd C #1183.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  double M[12][12];
+  char O;
+  double soma;
+  soma = 0;
+  scanf(" %c", &O);
+  for (int i = 0; i < 12; i++) {
+    for (int j = 0; j < 12; j++) {
+      scanf("%lf", &M[i][j]);
+    }
+  }
+  for (int i = 0; i < 12; i++) {
+    for (int j = i + 1; j < 12; j++) { //j > i = acima da diagonal principal
+      soma = soma + M[i][j];
+    }
+  }
+  if (O == 'S') {
+    printf("%.1lf\n", soma);
+  } else if (O == 'M') {
+    printf("%.1lf\n", soma/66);
+  }
+  return 0;
+}
